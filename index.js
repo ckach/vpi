0129921e2a9378be8a316fcaccf34a4e80720660
Ckach <ayomideresearch@gmail.com>
2022-12-10 17:36:09 +0400

    first commit

--- index.js
+++ index.js
@@ -236,7 +236,7 @@ async function main() {
 
 
 //PORT
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Listen on port ${port}`))
 
 
